0093dc3f97e07252d764b1909363fb7d5ae936af
Mario Wagner <ma_wa@gmx.at>
2020-02-10 13:11:34 +0100

    Added the for-loops

--- src/com/codersbay/repetitions/Aufgabe5_2_Repetitions.java
+++ src/com/codersbay/repetitions/Aufgabe5_2_Repetitions.java
@@ -2,6 +2,13 @@ package com.codersbay.repetitions;
 
 public class Aufgabe5_2_Repetitions {
     public static void main(String[] args) {
-
+        int result;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("times " + i + ":");
+            for (int j = 1; j <= 10; j++) {
+                result = j * i;
+                System.out.println(j + "*" + i + "=" + result);
+            }
+        }
     }
 }
